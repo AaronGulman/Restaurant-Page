b325dc56ed0f53bd98d88254b1da40ff68037763
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-02 21:35:22 -0500

    Add menuPg function(draft)

---
+++ src/extra modules/menu.js
@@ -0,0 +1,7 @@
+export function menuPg(){
+	document.body.innerHTML = '';
+
+	const articleOne = document.createElement('p')
+	document.body.appendChild(articleOne);
+	articleOne.textContent = "Our menu is a culinary masterpiece, carefully crafted to tantalize your taste buds and satisfy your cravings. From the moment you take your first bite of our crispy calamari, you'll be transported to a realm of unparalleled flavor and delight." 
+}
