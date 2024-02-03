d4aded545a8b10cd06847e5919b4d07212d6a3c9
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-02 21:31:20 -0500

    Add new JS modules: home,about,menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import { alertIt } from './extra modules/test';
+import { homePg } from './extra modules/home';
+import { menuPg } from './extra modules/menu';
+import { aboutPg } from './extra modules/about';
+
 import './index.css';
 
+const homePage = document.querySelector('#homeBtn')
+const menuPage = document.querySelector('#menuBtn')
+const aboutPage = document.querySelector('#aboutBtn')
 
-alertIt()
+
+homePg()
+
+
+homePage.addEventListener('click', () =>{
+console.log('clicked')
+	homePg()
+})
+
+menuPage.addEventListener('click', () =>{
+	console.log('clicked')
+
+	menuPg()
+})
+
+aboutPage.addEventListener('click', ()=>{
+	console.log('clicked')
+
+
+	aboutPg()
+})
+
 
 
