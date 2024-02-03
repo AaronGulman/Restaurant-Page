f40ff7f94fc630c149710b245482ecb1942286b7
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-02 21:33:17 -0500

    Add aboutPg function(draft)

---
+++ src/extra modules/about.js
@@ -0,0 +1,7 @@
+export function aboutPg(){
+	document.body.innerHTML = '';
+
+	const articleOne = document.createElement('p')
+	document.body.appendChild(articleOne);
+	articleOne.textContent = "At Urban Bites, we're more than just a restaurant – we're a culinary experience. Nestled in the heart of the city, our cozy yet vibrant space welcomes guests to indulge in a fusion of flavors from around the world." 
+}
