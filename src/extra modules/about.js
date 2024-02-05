ae3458e43e33f437668b5a48099c025139e027cc
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-04 20:17:55 -0500

    Add properties of the aboutPg function

--- src/extra modules/about.js
+++ src/extra modules/about.js
@@ -1,7 +1,10 @@
 export function aboutPg(){
-	document.body.innerHTML = '';
-
+	const content = document.querySelector("#content")
+	content.innerHTML = '';
+	const menuTitle = document.createElement('h1');
+	content.appendChild(menuTitle);
+	menuTitle.textContent = "Contact us";
 	const articleOne = document.createElement('p')
-	document.body.appendChild(articleOne);
+	content.appendChild(articleOne);
 	articleOne.textContent = "At Urban Bites, we're more than just a restaurant – we're a culinary experience. Nestled in the heart of the city, our cozy yet vibrant space welcomes guests to indulge in a fusion of flavors from around the world." 
 }
