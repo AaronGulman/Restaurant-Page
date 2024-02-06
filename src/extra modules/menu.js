579635843a0b96aee394d48392288b77801a30d7
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-06 00:36:32 -0500

    Create boxes for each item of the meny and add img,h,p to each of them

--- src/extra modules/menu.js
+++ src/extra modules/menu.js
@@ -38,40 +38,103 @@ export function menuPg(){
 
 
 	//img2
+	let imgBox2 = document.createElement('div')
+	imgBox2.setAttribute('class','menuBox')
 	let img2 = document.createElement('img')
 	img2.setAttribute('class','size')
 	img2.src = "/src/assets/food/hotdog.png"
 	content.appendChild(img2);
 
+	let img2Text = document.createElement('p')
+	img2Text.textContent = "Hello";
+
+	let img2Header = document.createElement('h1')
+	img2Header.textContent = "Hotdog";
+
+
+	content.appendChild(imgBox2);
+	imgBox2.appendChild(img2);
+	imgBox2.appendChild(img2Header);
+	imgBox2.appendChild(img2Text);
+
 	
 	//img3
 
+	let imgBox3 = document.createElement('div')
+	imgBox3.setAttribute('class','menuBox')
 	let img3 = document.createElement('img')
 	img3.setAttribute('class','size')
 	img3.src = "/src/assets/food/French_Fries.png"
 	content.appendChild(img3);
 
+	let img3Text = document.createElement('p')
+	img3Text.textContent = "Hello";
+	let img3Header = document.createElement('h1')
+	img3Header.textContent = "French Fries";
+
+
+	content.appendChild(imgBox3);
+	imgBox3.appendChild(img3);
+	imgBox3.appendChild(img3Header);
+	imgBox3.appendChild(img3Text);
+
 	//img4
 
+	let imgBox4 = document.createElement('div')
+	imgBox4.setAttribute('class','menuBox')
 	let img4 = document.createElement('img')
 	img4.setAttribute('class','size')
 	img4.src = "/src/assets/food/Nuggets.png"
 	content.appendChild(img4);
 
+	let img4Text = document.createElement('p')
+	img4Text.textContent = "Hello";
+	let img4Header = document.createElement('h1')
+	img4Header.textContent = "Nuggets";
+
+	content.appendChild(imgBox4);
+	imgBox4.appendChild(img4);
+	imgBox4.appendChild(img4Header);
+	imgBox4.appendChild(img4Text);
+
 	//img5
 
+	let imgBox5 = document.createElement('div')
+	imgBox5.setAttribute('class','menuBox')
 	let img5 = document.createElement('img')
 	img5.setAttribute('class','size')
 	img5.src = "/src/assets/food/pizza.png"
 	content.appendChild(img5);
 
+	let img5Text = document.createElement('p')
+	img5Text.textContent = "Hello";
+	let img5Header = document.createElement('h1')
+	img5Header.textContent = "Pizza";
+
+	content.appendChild(imgBox5);
+	imgBox5.appendChild(img5);
+	imgBox5.appendChild(img5Header);
+	imgBox5.appendChild(img5Text);
+
 	//img6
 
 
+	let imgBox6 = document.createElement('div')
+	imgBox6.setAttribute('class','menuBox')
 	let img6 = document.createElement('img')
 	img6.setAttribute('class','size')
 	img6.src = "/src/assets/food/taco.png"
 	content.appendChild(img6);
+
+	let img6Text = document.createElement('p')
+	img6Text.textContent = "Hello";
+	let img6Header = document.createElement('h1')
+	img6Header.textContent = "Taco";
+
+	content.appendChild(imgBox6);
+	imgBox6.appendChild(img6);
+	imgBox6.appendChild(img6Header);
+	imgBox6.appendChild(img6Text);
 
 
 
