33278ec7b58e1c4b3125259164d858598800e082
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-17 21:14:18 -0500

    Erase unnecessary white spaces

--- src/extra modules/about.js
+++ src/extra modules/about.js
@@ -11,5 +11,5 @@ export function aboutPg(){
 	iframe.setAttribute('id','map')
 	iframe.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6159.572460581201!2d-79.38687309608002!3d43.65126715809759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sco!4v1707443919432!5m2!1sen!2sco'
 	content.appendChild(iframe)
-
 }
+
--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import { homePg } from './extra modules/home';
 import { menuPg } from './extra modules/menu';
 import { aboutPg } from './extra modules/about';
-
 import './index.css';
 
 const homePage = document.querySelector('#homeBtn')
 const menuPage = document.querySelector('#menuBtn')
 const aboutPage = document.querySelector('#aboutBtn')
 
-
 homePg()
 
-
+//add home elements
 homePage.addEventListener('click', () =>{
 console.log('clicked')
 	homePg()
 })
 
+//fix adding menu elemets every time the button is clicked
 menuPage.addEventListener('click', () =>{
 	console.log('clicked')
-
 	menuPg()
 })
 
+//add about elements
+
 aboutPage.addEventListener('click', ()=>{
 	console.log('clicked')
-
-
 	aboutPg()
 })
 
